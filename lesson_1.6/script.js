b1c4b2702f6f8fefde831999adad3a858ce25fa9
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-08-14 23:44:08 +0600

    lesson 1.6 practice done

--- lesson_1.6/script.js
+++ lesson_1.6/script.js
@@ -1,14 +1,52 @@
+const counters = document.getElementById("counters");
 const incrementBtn = document.getElementById("increment");
 const decrementBtn = document.getElementById("decrement");
-const count = document.getElementById("count");
 
 //initial state
 const initialState = {
-    value : 0;
-}
-
+  value: 0,
+};
 
 //Reducer function
-function counterReducer (state = initialState, action){
-    if(action = )
+function counterReducer(state = initialState, action) {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return state;
+  }
 }
+
+//create store
+const store = Redux.createStore(counterReducer);
+
+//subscribe store
+const render = () => {
+  const state = store.getState();
+  counters.innerText = state.value.toString();
+};
+
+// update UI initially
+render();
+
+store.subscribe(render);
+
+//button click events
+incrementBtn.addEventListener("click", () => {
+  store.dispatch({
+    type: "increment",
+  });
+});
+
+decrementBtn.addEventListener("click", () => {
+  store.dispatch({
+    type: "decrement",
+  });
+});
