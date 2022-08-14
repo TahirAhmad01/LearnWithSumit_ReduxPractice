1042e457dccaf54aedba2c757525c6c1a9158c3f
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-08-14 22:31:29 +0600

    first commit

---
+++ lesson_1.6/script.js
@@ -0,0 +1,14 @@
+const incrementBtn = document.getElementById("increment");
+const decrementBtn = document.getElementById("decrement");
+const count = document.getElementById("count");
+
+//initial state
+const initialState = {
+    value : 0;
+}
+
+
+//Reducer function
+function counterReducer (state = initialState, action){
+    if(action = )
+}
